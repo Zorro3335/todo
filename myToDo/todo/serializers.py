323153ce734db40fa2de45e181f02068d811e49c
Zorro3335 <123104276+Zorro3335@users.noreply.github.com>
2024-05-15 10:14:36 +0300

    add

--- myToDo/todo/serializers.py
+++ myToDo/todo/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from .models import ToDo
+from django.contrib.auth.models import User
 
 
 class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    todo = ToDoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'password', 'username', 'todo']
