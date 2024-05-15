9c2681c285ca6c1d15ec64a5c9bcd96592578025
Zorro3335 <123104276+Zorro3335@users.noreply.github.com>
2024-05-15 10:20:17 +0300

    Update settings.py

--- myToDo/myToDo/settings.py
+++ myToDo/myToDo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zbh2yd7g42f-z#0gw^87_pgg!g3_ov(5%c2a$28=7o)=z174s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vas.pythonanywhere.com']
 
 
 # Application definition
